63bddaa00dcee4f0f22b18e57b75baddbbaafafc
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-01-17 11:51:34 +0000

    edited the models

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -21,29 +22,30 @@ class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
+
     MEMBERSHIP_CHOICES = [
-        (MEMBERSHIP_BRONZE, 'Bronze')
-        (MEMBERSHIP_SILVER, 'Silver')
-        (MEMBERSHIP_GOLD, 'Gold')
-    ],
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_BRONZE)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES)
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED = 'F'
     PAYMENT_STATUS_CHOICES = [
-        (PAYMENT_STATUS_PENDING, 'Pending')
-        (PAYMENT_STATUS_COMPLETE, 'Complete')
-        (PAYMENT_STATUS_FAILED, 'Failed')
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 class OrderItem(models.Model):
@@ -56,9 +58,6 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-
-
-
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
