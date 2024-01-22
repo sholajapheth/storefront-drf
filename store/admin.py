e810471926f2c9b3248b9ff9c5559cb6d5949efc
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-01-22 12:55:28 +0000

    added notes on admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+
+
 
 # Register your models here.
+admin.site.register(models.Collection)
+admin.site.register(models.Product, ProductAdmin)
