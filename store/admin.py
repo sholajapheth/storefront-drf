92abe65d55511bccd548afa9277cec1b497d8ea5
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-01 07:39:25 +0000

    changed some admin models

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,33 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 
 from . import models
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventor_lt=10)
 
 class ProductAdmin(admin.ModelAdmin):
+    # exclude = ['title', 'slug'] excludes these fields in the form
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection'] #we can use the below function to do the same thing
     list_filter = ['collection__id']
@@ -22,6 +41,17 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
+    # creating custome actions
+    @admin.action(description="Clear Inventory")
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f"{updated_count} products were successfully updated",
+            messages.ERROR
+        )
+
+
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
@@ -29,14 +59,23 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
+class OrderItemInline(admin.TabularInline):
+    #autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
+    inlines = [OrderItemInline]
     ordering = ['placed_at']
     list_per_page = 10
     list_select_related = ['customer']
 
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
