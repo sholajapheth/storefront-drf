ad5dc485b51537d1df638ff44f7dd39df2f54625
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-09 07:09:02 +0000

    deserializing

--- store/views.py
+++ store/views.py
@@ -6,11 +6,18 @@ from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('ok')
+
 
 @api_view()
 def product_detail(request, id):
