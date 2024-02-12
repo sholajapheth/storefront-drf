67ff1c7354e7894c4231336c3e0acc9a353d049d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-12 00:21:45 +0000

    feat: products can be filtered by collection

--- store/views.py
+++ store/views.py
@@ -12,8 +12,16 @@ from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        
+        return queryset
 
     def get_serializer_context(self):
         return {'request': self.request}
