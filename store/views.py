dad6f49aa443f3027e8b14d57ca9558e6246ef8b
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-14 07:31:17 +0000

    fix: remove unused imports

--- store/views.py
+++ store/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from django.db.models import Count
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
