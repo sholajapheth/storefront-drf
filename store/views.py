a3e0d06b146d0790ef7f56844a4ae5e15cbd90f0
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-14 19:04:02 +0000

    feat: add functionality for deleting a cart

--- store/views.py
+++ store/views.py
@@ -2,11 +2,10 @@ from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.generics import RetrieveAPIView
 from .models import Product, Collection, OrderItem, Review, Cart
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer
 from .filters import ProductFilter
@@ -58,6 +57,6 @@ class ReviewViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
 
-class CartViewSet(CreateModelMixin, RetrieveAPIView, GenericViewSet):
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
