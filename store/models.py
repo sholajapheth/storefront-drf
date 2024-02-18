48bf5684a81957c0dfcf6c6647902bcafbe5901a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-18 16:06:47 +0000

    feat: build the user profile api

--- store/models.py
+++ store/models.py
@@ -58,6 +58,7 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
 
--- store/serializers.py
+++ store/serializers.py
@@ -1,9 +1,6 @@
-from rest_framework import serializers
 from decimal import Decimal
-
-from rest_framework.exceptions import ValidationError
-
-from store.models import Product, Collection, Review, Cart, CartItem
+from rest_framework import serializers
+from store.models import Product, Collection, Review, Cart, CartItem, Customer
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -100,3 +97,11 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ['quantity']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Customer
+        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
