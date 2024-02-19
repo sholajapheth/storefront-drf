77115f5f1023ff480037d17c49ef8827014c1de9
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-19 17:35:09 +0000

    fix: add signals to remove app depending on each other

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,10 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
+from store.models import Customer
+
 
 class UserCreateSerializer(BaseUserCreateSerializer):
+
+
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
 
