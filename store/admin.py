6644dc7d994baf57805d68a8693ca0f6495578bd
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-23 18:38:15 +0000

    refactor: handle product image in the admin panel, and refactored the entire admin registration logic

--- store/admin.py
+++ store/admin.py
@@ -1,41 +1,62 @@
 from django.contrib import admin, messages
-from django.db.models import Count
-from django.urls import reverse
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
 from django.utils.html import format_html, urlencode
-
+from django.urls import reverse
 from . import models
 
 
+# from tags.models import Tag
+
+
+# @admin.register(Tag)
+# class TagAdmin(admin.ModelAdmin):
+#     pass
+
+
 class InventoryFilter(admin.SimpleListFilter):
-    title = "inventory"
-    parameter_name = "inventory"
+    title = 'inventory'
+    parameter_name = 'inventory'
 
     def lookups(self, request, model_admin):
         return [
             ('<10', 'Low')
         ]
 
-    def queryset(self, request, queryset):
+    def queryset(self, request, queryset: QuerySet):
         if self.value() == '<10':
-            return queryset.filter(inventor_lt=10)
+            return queryset.filter(inventory__lt=10)
 
 
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instance):
+        if instance.image.name != '':
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail"/>')
+        return ''
+
+
+
+@admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    # exclude = ['title', 'slug'] excludes these fields in the form
     autocomplete_fields = ['collection']
     prepopulated_fields = {
         'slug': ['title']
     }
     actions = ['clear_inventory']
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    inlines = [ProductImageInline]
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
-    list_select_related = ['collection']  # we can use the below function to do the same thing
-    list_filter = ['collection__id']
+    list_select_related = ['collection']
+    search_fields = ['title']
 
-    # def collection_title(self, product):
-    #     return product.collection.title
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -43,41 +64,24 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
-    # creating custome actions
-    @admin.action(description="Clear Inventory")
+    @admin.action(description='Clear inventory')
     def clear_inventory(self, request, queryset):
         updated_count = queryset.update(inventory=0)
         self.message_user(
             request,
-            f"{updated_count} products were successfully updated",
+            f'{updated_count} products were successfully updated.',
             messages.ERROR
         )
 
-
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
-    list_editable = ['membership']
-    list_per_page = 10
-    list_select_related = ['user']
-    ordering = ['user__first_name', 'user__last_name']
-    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    class Media:
+        css = {
+            'all': ['store/styles.css']
+        }
 
 
-class OrderItemInline(admin.TabularInline):
-    min_num = 1
-    max_num = 10
-    model = models.OrderItem
-
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'placed_at', 'customer']
-    inlines = [OrderItemInline]
-    ordering = ['placed_at']
-    list_per_page = 10
-    list_select_related = ['customer']
-
-
+@admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['featured_product']
     list_display = ['title', 'products_count']
     search_fields = ['title']
 
@@ -89,8 +93,7 @@ class CollectionAdmin(admin.ModelAdmin):
                 + urlencode({
             'collection__id': str(collection.id)
         }))
-        '''returning an html content eg: link, imported at the top'''
-        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+        return format_html('<a href="{}">{} Products</a>', url, collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
@@ -98,8 +101,41 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
-# Register your models here.
-admin.site.register(models.Collection, CollectionAdmin)
-admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.Customer, CustomerAdmin)
-admin.site.register(models.Order, OrderAdmin)
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
+    list_editable = ['membership']
+    list_per_page = 10
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+                reverse('admin:store_order_changelist')
+                + '?'
+                + urlencode({
+            'customer__id': str(customer.id)
+        }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = ['id', 'placed_at', 'customer']
